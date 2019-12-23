e9a9714a1f32e86de8c7753adc37ea1195ad46af
Quang Nguyen <qmnguyenw@gmail.com>
2019-12-23 03:32:22 -0500

    Integrating Angular with Auth0

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,5 @@
+// 1st
+
 // import { Component } from '@angular/core';
 
 // @Component({
@@ -9,35 +11,53 @@
 //   title = 'frontend';
 // }
 
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs';
-import {ExamsApiService} from './exams/exams-api.service';
-import {Exam} from './exams/exam.model';
+
+// 2nd
+// import {Component, OnInit, OnDestroy} from '@angular/core';
+// import {Subscription} from 'rxjs';
+// import {ExamsApiService} from './exams/exams-api.service';
+// import {Exam} from './exams/exam.model';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements OnInit, OnDestroy {
+//   title = 'app';
+//   examsListSubs: Subscription;
+//   examsList: Exam[];
+
+//   constructor(private examsApi: ExamsApiService) {
+//   }
+
+//   ngOnInit() {
+//     this.examsListSubs = this.examsApi
+//       .getExams()
+//       .subscribe(res => {
+//           this.examsList = res;
+//         },
+//         console.error
+//       );
+//   }
+
+//   ngOnDestroy() {
+//     this.examsListSubs.unsubscribe();
+//   }
+// }
+
+// 3rd
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div style="text-align:center">
+      <h1>Exams</h1>
+    </div>
+    <h2>Here are the exams created so far: </h2>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'app';
-  examsListSubs: Subscription;
-  examsList: Exam[];
-
-  constructor(private examsApi: ExamsApiService) {
-  }
-
-  ngOnInit() {
-    this.examsListSubs = this.examsApi
-      .getExams()
-      .subscribe(res => {
-          this.examsList = res;
-        },
-        console.error
-      );
-  }
-
-  ngOnDestroy() {
-    this.examsListSubs.unsubscribe();
-  }
-}
+export class AppComponent { }
--- frontend/src/app/exams/exams-api.service.ts
+++ frontend/src/app/exams/exams-api.service.ts
@@ -1,30 +1,42 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/catch';
-import {throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
-import {API_URL} from '../env';
-import {Exam} from './exam.model';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { API_URL } from '../env';
+import { Exam } from './exam.model';
+import * as Auth0 from 'auth0-web';
 
 @Injectable()
 export class ExamsApiService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {
-    }
+  private static _handleError(err: HttpErrorResponse | any) {
+    return throwError(err.message || 'Error: Unable to complete request."');
+  }
 
-    private static _handleError(err: HttpErrorResponse | any) {
-        return throwError(err.message || 'Error: Unable to complete request.');
-    }
+  // GET list of public, future events
+  getExams(): Observable<Exam[]> {
+    // return this.http
+    //         .get(`${API_URL}/exams`)
+    //         .catch(ExamsApiService._handleError);
+    return this.http
+      .get<Exam[]>(`${API_URL}/exams`)
+      .pipe(catchError(ExamsApiService._handleError));
+  }
 
-    // GET list of public, future events
-    getExams(): Observable<Exam[]> {
-        // return this.http
-        //         .get(`${API_URL}/exams`)
-        //         .catch(ExamsApiService._handleError);
-        return this.http.get<Exam[]>(`${API_URL}/exams`).pipe(
-            catchError(ExamsApiService._handleError)
-        );
-    }
-
+  saveExam(exam: Exam): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${Auth0.getAccessToken()}`
+      })
+    };
+    return this.http
+      .post(`${API_URL}/exams`, exam, httpOptions);
+  }
 }
